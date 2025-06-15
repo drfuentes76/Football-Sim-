8b9fc86da919252c931f5a985534e46e46745cce
drfuentes76 <drfuentes76@gmail.com>
2025-06-15 15:30:18 -0500

    Delete nes_emulator.js

--- nes_emulator.js
+++
@@ -1,4 +0,0 @@
-// Emulator script placeholder
-function pressButton(btn) { console.log('Pressed:', btn); }
-function saveState() { alert('State saved'); }
-function loadState() { alert('State loaded'); }
