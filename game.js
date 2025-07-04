76dcaebb24f0f434c2f84ee8f6d02a8eb5e714ae
drfuentes76 <drfuentes76@gmail.com>
2025-07-04 15:30:36 -0500

    Add files via upload

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
-let score=[0,0];function play(t){let g=Math.floor(Math.random()*10)+1;if(t==='pass'){g+=Math.floor(Math.random()*5);}
+let score=[0,0];function play(t){
+let g=Math.floor(Math.random()*10)+1;if(t==='pass'){g+=Math.floor(Math.random()*5);}
 score[0]+=g>7?7:0;document.getElementById('score').textContent=`${score[0]} - ${score[1]}`;
 const log=document.getElementById('log');log.innerHTML+=`Play: ${t} gained ${g} yards.<br>`;}
